a2951b4c2e1832dec764461932155bb24714aaff
Saurabh Gautam <saurabhgautam567801@gmail.com>
2024-12-28 15:06:43 +0530

    Initial commit - Music Player Project

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = 3000;
 
-// Serve static files (HTML, CSS, JS, audio)
+const app = express();
+const PORT = 3000;
+
+// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Home route
+// Define routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
